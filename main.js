f543ef228867b203f812492c524eb6ecba056010
saeedkaboos <112353222+saeedkaboos@users.noreply.github.com>
2025-12-01 23:52:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+ 
+ function summ(){
+ const a=document.getElementById("a").value
+ const b=document.getElementById("b").value
+ const c=document.getElementById("c")
+c.value=parseInt(a)+parseInt(b);
+    }
+ function subb(){
+ const a=document.getElementById("a").value
+ const b=document.getElementById("b").value
+ const c=document.getElementById("c")
+c.value=parseInt(a)-parseInt(b);
+    }
+ function multt(){
+ const a=document.getElementById("a").value
+ const b=document.getElementById("b").value
+ const c=document.getElementById("c")
+c.value=parseInt(a)*parseInt(b);
+    }
+    
+ function divv(){
+ const a=document.getElementById("a").value
+ const b=document.getElementById("b").value
+ const c=document.getElementById("c")
+c.value=parseFloat(a)/parseFloat(b);
+    }
